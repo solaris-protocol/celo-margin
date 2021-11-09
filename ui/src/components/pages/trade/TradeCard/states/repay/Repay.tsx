a006c485ab5ec7d83c9db281e17976de39dedd90
Alexey Elizarov <felex-ae@ya.ru>
2021-11-09 20:17:23 +0300

    chore: repay token

--- ui/src/components/pages/trade/TradeCard/states/repay/Repay.tsx
+++ ui/src/components/pages/trade/TradeCard/states/repay/Repay.tsx
@@ -2,6 +2,7 @@ import { useContractKit } from '@celo-tools/use-contractkit'
 import React, { FC, useState } from 'react'
 import styled from 'styled-components'
 
+import { useTrade } from '../../../../../../app/contexts/trade'
 import { useToggleWalletSelectModal } from '../../../../../../state/application/hooks'
 import { Button } from '../../common/Button'
 import { PayRepay } from '../../common/PayRepay'
@@ -21,12 +22,13 @@ interface Props {
 export const Repay: FC<Props> = ({ state, setState }) => {
   const { address: wallet } = useContractKit()
   const toggleWalletSelectModal = useToggleWalletSelectModal()
+  const { tokenA } = useTrade()
 
   const [value, setValue] = useState('')
 
   return (
     <TradeBody>
-      <PayRepay value={value} setValue={setValue} state={state} setState={setState} />
+      <PayRepay token={tokenA} value={value} setValue={setValue} state={state} setState={setState} />
       <Params />
       {!wallet ? (
         <ButtonStyled onClick={toggleWalletSelectModal}>Connect wallet</ButtonStyled>
